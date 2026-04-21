c44e125fde4a35041c56bf633154f5088a73cb0f
eryelberdesouza-spec <eryelberdesouza@gmail.com>
2026-04-20 22:17:04 -0300

    Create trpcClient.ts

---
+++ client/src/lib/trpcClient.ts
@@ -0,0 +1,17 @@
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import superjson from "superjson";
+import type { AppRouter } from "../../../server/routers";
+
+export const trpc = createTRPCReact<AppRouter>();
+
+export function createTRPCClient() {
+  return trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: "/api/trpc",
+        transformer: superjson,
+      }),
+    ],
+  });
+}
